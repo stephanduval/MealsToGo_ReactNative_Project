c34f5ac698e70e1851a169e38b457d25f5005679
Stephan DuVal <stephan.duval@gmail.com>
2023-01-11 15:35:50 -0700

    Fixed ESLINT ISSUES 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'object-curly-spacing': ['error', 'always'],
     'react-native/no-inline-styles': 0,
     'object-curly-spacing': 'off',
   },
